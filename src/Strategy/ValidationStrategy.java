48ac7c50e43c67fd4797c92e9e9c429ff1152afd
Sebastian Christiansen <seba4746@stud.kea.dk>
2018-11-08 15:03:31 +0100

    Implemented strategy pattern for following validation classes: IPv4Validator, NameValidator, PortValidator

---
+++ src/Strategy/ValidationStrategy.java
@@ -0,0 +1,36 @@
+package Strategy;
+
+public interface MessageStrategy
+{
+    void messageStrategy();
+}
+
+class SendMessageStrategy implements MessageStrategy
+{
+
+    @Override
+    public void messageStrategy()
+    {
+
+    }
+}
+
+class ReceiveMessageStrategy implements MessageStrategy
+{
+
+    @Override
+    public void messageStrategy()
+    {
+
+    }
+}
+
+class ImAliveMessageStrategy implements MessageStrategy
+{
+
+    @Override
+    public void messageStrategy()
+    {
+
+    }
+}
